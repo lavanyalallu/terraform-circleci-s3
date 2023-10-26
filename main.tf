e346c13380035a7bab6baf49fbf04c1211fb92c8
lavanyaibex <lavanya.govada@ibexlabs.com>
2023-10-26 19:06:41 +0530

    circle ci

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.12.1"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1" # define region as per your account
+}
+
+resource "aws_s3_bucket" "new_bucket" {
+  bucket = "lavanya-demo-circleci-999"
+
+  object_lock_enabled = false
+
+  tags = {
+    Environment = "Prod"
+  }
+}
